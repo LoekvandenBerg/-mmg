4fd5fef6ec832a4ca62c619f2ef4cfd635fef9f5
Loek <L_V_D_BERG@hotmail.com>
2020-03-20 11:38:01 +0100

    Added a menu

---
+++ Management Game/Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    private Button orcButton, humanButton;
+
+    public void OnOrcButton()
+    {
+        SceneManager.LoadScene("OrcScene");
+    }
+
+    public void OnHumanButton()
+    {
+        SceneManager.LoadScene("HumanScene");
+    }
+
+    public void OnNOtYetImplementedButton()
+    {
+        throw new System.NotImplementedException();
+    }
+}
